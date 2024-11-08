9cfcc1958945754b5f2c58344e8593ec8fb413d5
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-08 15:50:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const knex = require("knex")({
  connection: {
   host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
   user: "admin",
-  password: "Password1",
-  database:"paradise-concerts",
+  password: "pokem0n1stheb3st",
+  database:"pokedb",
   port: 3306,
  },
 });
