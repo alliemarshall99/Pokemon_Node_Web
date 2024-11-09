051dff36f656732c5a083cde3ed46a65630e5a1d
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-08 22:35:31 -0700

    Update index.js to correct endpoint and password

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 let express = require("express");
-let app = new express();                                                                     
-
+let app = new express();
+app.set("view engine","ejs")
 // set up database connection
 const knex = require("knex")({
  client: "mysql",
  connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+  host:"pokemon-db.c1g0y6ay8poy.us-east-2.rds.amazonaws.com",
   user: "admin",
-  password: "pokem0n1stheb3st",
+  password: "Password1",
   database:"pokedb",
   port: 3306,
  },
 });
-
 app.get("/",(req,res) => {
- knex
+knex
  .select()
  .from("venues")
  .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+  res.render("index", {aConcerts: result});
+ });
 });
 app.listen(3000);
