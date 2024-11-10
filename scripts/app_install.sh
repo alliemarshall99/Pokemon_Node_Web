3858f105b5cc47001507e975d34594db75d5f879
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-09 22:57:26 -0700

    Update app_install.sh

--- scripts/app_install.sh
+++ scripts/app_install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Navigate to the app directory
+# Navigate to the application directory
 cd /home/ec2-user/app
 
-# Set up NVM and Node.js environment
+# Set up Node.js environment
 export NVM_DIR="/home/ec2-user/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # Load NVM
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # Load NVM bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# Install dependencies (if not already installed)
+# Install dependencies
 npm install
 
-# Ensure pm2 is installed globally
-npm install -g pm2
