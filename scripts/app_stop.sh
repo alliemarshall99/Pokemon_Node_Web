21e93de68e8ae0821ca84eaa3d3c8965257bd860
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-09 22:58:45 -0700

    Update app_stop.sh

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Set up NVM and Node.js environment
+# Set up Node.js environment
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# Stop the application by name (consistent with app_start.sh)
-/usr/bin/pm2 stop "pokemon_app" || true  # Ignore error if the app is not running
+# Stop the application
+pm2 stop "index" || echo "App is not running"
