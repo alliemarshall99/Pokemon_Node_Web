5abae089d1899a99ebdf43ae376914dfe2bda8fe
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-09 22:47:12 -0700

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Navigate to the app directory
 cd /home/ec2-user/app
-export NVM_DIR="/home/ec2-user/.nvm" 
+
+# Set up NVM and Node.js environment
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-pm2 start index.js
+
+# Start the application using pm2 with the full path
+/usr/bin/pm2 start index.js --name "pokemon_app"  # Assigns a name for easy reference in pm2
