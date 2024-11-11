24e50acba3ac7b34fa66d12b0bac2fb6d829a22e
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-11 11:25:12 -0700

    Update app_install.sh

--- scripts/app_install.sh
+++ scripts/app_install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Navigate to the application directory
 cd /home/ec2-user/app
-
-# Set up Node.js environment
-export NVM_DIR="/home/ec2-user/.nvm"
+export NVM_DIR="/home/ec2-user/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-# Install dependencies
-npm install
-
+npm install 
