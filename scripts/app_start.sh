aace8966b84fc2a257086c02e5c0ed5f3df4ac9b
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-11 11:25:30 -0700

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# Navigate to the application directory
 cd /home/ec2-user/app
-
-# Set up Node.js environment
-export NVM_DIR="/home/ec2-user/.nvm"
+export NVM_DIR="/home/ec2-user/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-# Start the app with pm2
-pm2 start /home/ec2-user/app/index.js --name "index" || pm2 restart "index"
+pm2 start index.js
