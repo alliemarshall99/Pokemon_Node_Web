7adc00416b637ed314b8b8fb3ac967599ec2aad3
Allie Marshall Shumway <131930407+alliemarshall99@users.noreply.github.com>
2024-11-11 11:25:46 -0700

    Update app_stop.sh

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Set up Node.js environment
-export NVM_DIR="/home/ec2-user/.nvm"
+cd /home/ec2-user/app
+export NVM_DIR="/home/ec2-user/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-# Stop the application
-pm2 stop "index" || echo "App is not running"
+pm2 stop index.js
